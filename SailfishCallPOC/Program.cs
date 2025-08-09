ad113571dc329616459f7bc763fa6d598f91003a
sarita kumari <37065221+kumarisarita5@users.noreply.github.com>
2025-08-09 08:21:07 +0530

    Add files via upload

---
+++ SailfishCallPOC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Identity.Client;
+using Newtonsoft.Json;
+
+namespace SailfishCallPOC
+{
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! From Sailfish Call POC Project");
+
+            var armService = new AzureArmService("1f847e97-a9e0-4335-9e1a-531c116d57e0");
+
+            var res = armService.GetResourceGroupsAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Resource Groups: " + JsonConvert.SerializeObject(res));
+        }
+    }
+}
